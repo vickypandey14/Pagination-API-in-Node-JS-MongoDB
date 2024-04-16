d9cee0a7d7ba70c9afe33dc2fabf1f2fabde76f5
vickypandey14 <vickypandeyts@gmail.com>
2024-04-16 10:01:28 +0530

    new route created

--- server.js
+++ server.js
@@ -1,2 +1,36 @@
 const express = require('express')
 const app = express()
+
+const users = [
+    {
+        id: 1,
+        name: "vicky",
+        age: 20
+    },
+    {
+        id: 2,
+        name: "User 2",
+        age: 22
+    },
+    {
+        id: 3,
+        name: "User 3",
+        age: 21
+    },
+    {
+        id: 4,
+        name: "User 4",
+        age: 17
+    },
+    {
+        id: 5,
+        name: "User 5",
+        age: 19
+    }
+]
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+app.listen(3000)
