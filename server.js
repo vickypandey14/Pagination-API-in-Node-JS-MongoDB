bcb74d70a2d9e8e549efdb61960896cdce0c4f3e
vickypandey14 <vickypandeyts@gmail.com>
2024-04-17 19:03:14 +0530

    Users Pagination API Done working on other API's

--- server.js
+++ server.js
@@ -1,36 +1,46 @@
 const express = require('express')
 const app = express()
 
-const users = [
-    {
-        id: 1,
-        name: "vicky",
-        age: 20
-    },
-    {
-        id: 2,
-        name: "User 2",
-        age: 22
-    },
-    {
-        id: 3,
-        name: "User 3",
-        age: 21
-    },
-    {
-        id: 4,
-        name: "User 4",
-        age: 17
-    },
-    {
-        id: 5,
-        name: "User 5",
-        age: 19
-    }
-]
+const users = [];
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => {
+    users.push(...data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+});
 
 app.get('/users', (req, res) => {
-    res.json(users)
+    const page = parseInt(req.query.page)
+    const limit = parseInt(req.query.limit)
+
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+
+    const results = {}
+
+    // for the next page
+    
+    if (endIndex < users.length) {
+        results.next = {
+            page: page + 1,
+            limit: limit
+        }
+    }
+
+    // for the previous page
+    
+    if (startIndex > 0) {
+        results.previous = {
+            page: page - 1,
+            limit: limit
+        }
+    }
+
+    results.results = users.slice(startIndex, endIndex)
+    res.json(results)
 })
 
 app.listen(3000)
