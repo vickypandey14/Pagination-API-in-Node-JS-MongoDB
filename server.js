b78e5465ae5d41d3d77515143be4bf428e349921
vickypandey14 <vickypandeyts@gmail.com>
2024-04-19 23:44:35 +0530

    mongo db connect

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const User = require('./models/users')
+
+mongoose.connect('mongodb://localhost/pagination-api-nodejs')
+const db = mongoose.connection
+
+db.once('open', async() => {
+    if (await User.countDocuments(). exec() > 0) return
+
+    Promise.all([
+        User.create({ name: 'User 1', }),
+        User.create({ name: 'User 2', }),
+        User.create({ name: 'User 3', }),
+        User.create({ name: 'User 4', }),
+        User.create({ name: 'User 5', }),
+        User.create({ name: 'User 6', }),
+        User.create({ name: 'User 7', }),
+        User.create({ name: 'User 8', }),
+        User.create({ name: 'User 9', }),
+        User.create({ name: 'User 10', }),
+        User.create({ name: 'User 11', }),
+        User.create({ name: 'User 12', })
+    ]).then(() => console.log('Added Users'))
+})
 
 // for users API
 
@@ -15,7 +39,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
 });
 
 
-app.get('/users', paginatedResults(users), (req, res) => {
+app.get('/users', paginatedResults(User), (req, res) => {
     res.json(res.paginatedResults)
 })
 
@@ -39,7 +63,7 @@ app.get('/posts', paginatedResults(posts), (req, res) => {
 
 
 function paginatedResults(model){
-    return (req, res, next) => {
+    return async(req, res, next) => {
         const page = parseInt(req.query.page)
         const limit = parseInt(req.query.limit)
 
@@ -66,9 +90,14 @@ function paginatedResults(model){
             }
         }
 
-        results.results = model.slice(startIndex, endIndex)
-        res.paginatedResults = results
-        next()
+        try {
+            results.results = await model.find().limit(limit).skip(startIndex).exec()
+            res.paginatedResults = results
+            next()
+        }
+        catch (error) {
+            res.status(500).json({ message: e.message })
+        }        
     }
 }
 
